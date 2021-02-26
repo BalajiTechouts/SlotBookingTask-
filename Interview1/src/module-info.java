5ef97ddbd6516955a7dfd79f55d97d6bfda1eaef
D1 <D1@TO-83W6HL2-D.corp.techouts.local>
2021-02-26 16:45:58 +0530

    FirstCommit

---
+++ Interview1/src/module-info.java
@@ -0,0 +1,19 @@
+class Number()
+{
+	public static void main(String args[])
+	{
+	int n,rev=0;
+	System.out.println("Enter an integer");
+	while(n!=0)
+	{
+		remainder=n%10;
+		rev=rev*10+remainder;
+		n=n/10;
+	
+	}
+	System.out.println("The reversed integer is: rev");
+	Number r = new Number();
+	r.Number();
+	
+}
+}
